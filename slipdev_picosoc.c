90d5d5d7d2b4b65f24b666ae5a525abf7c0620c3
Graham Edgecombe <gpe@grahamedgecombe.com>
2018-04-06 21:28:14 +0100

    Add PicoSoC slipdev driver

---
+++ slipdev_picosoc.c
@@ -0,0 +1,26 @@
+#include "slipdev_picosoc.h"
+
+#include <stdint.h>
+#include <uip/slipdev.h>
+
+#define UART_CLKDIV (*(volatile uint32_t *) 0x02000004)
+#define UART_DATA   (*(volatile  int32_t *) 0x02000008)
+
+void slipdev_picosoc_init(void) {
+    UART_CLKDIV = 104; /* 115200 baud at 12 MHz */
+    slipdev_init();
+}
+
+void slipdev_char_put(uint8_t c) {
+    UART_DATA = c;
+}
+
+uint8_t slipdev_char_poll(uint8_t *c) {
+    int32_t data = UART_DATA;
+    if (data != -1) {
+        *c = data;
+        return 1;
+    } else {
+        return 0;
+    }
+}
---
+++ slipdev_picosoc.h
@@ -0,0 +1,6 @@
+#ifndef SLIPDEV_PICOSOC_H
+#define SLIPDEV_PICOSOH_H
+
+void slipdev_picosoc_init(void);
+
+#endif
