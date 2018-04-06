bed0c04584bb9c7f2dc762602ec9540e4f66f285
Graham Edgecombe <gpe@grahamedgecombe.com>
2018-04-06 21:29:43 +0100

    Comment httpd.h include

--- uipopt.h
+++ uipopt.h
@@ -554,7 +554,7 @@ struct httpd_state {
 /* Include the header file for the application program that should be
    used. If you don't use the example web server, you should change
    this. */
-#include "httpd.h"
+/*#include "httpd.h"*/
 
 
 #endif /* __UIPOPT_H__ */
