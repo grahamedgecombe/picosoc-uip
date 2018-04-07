65ea464c795b8791ebc0536ad136d2faf86c24af
Graham Edgecombe <gpe@grahamedgecombe.com>
2018-04-07 19:17:03 +0100

    Call uip_periodic() once per second

--- firmware.c
+++ firmware.c
@@ -7,11 +7,19 @@
 
 #define LEDS (*(volatile uint32_t *) 0x03000000)
 
+static inline uint32_t rdcycle(void) {
+    uint32_t cycle;
+    asm volatile ("rdcycle %0" : "=r"(cycle));
+    return cycle;
+}
+
 int main(void) {
     LEDS = 0xAA;
 
     slipdev_picosoc_init();
     uip_init();
+
+    uint32_t last_periodic = rdcycle();
 
     for (;;) {
         uip_len = slipdev_poll();
@@ -19,6 +27,18 @@ int main(void) {
             uip_input();
             if (uip_len > 0) {
                 slipdev_send();
+            }
+        }
+
+        uint32_t now = rdcycle();
+        if ((now - last_periodic) >= 12000000) {
+            last_periodic = now;
+
+            for (uint8_t i = 0; i < UIP_CONNS; i++) {
+                uip_periodic(i);
+                if (uip_len > 0) {
+                    slipdev_send();
+                }
             }
         }
     }
